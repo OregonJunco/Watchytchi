e205ddf767e5b56e3b7cfd0377930dad022732b7
Robin LoBuglio <B.LoBuglio@gmail.com>
2024-08-05 12:20:02 -0700

    Revert another debug setting

--- Watchytchiuno/DebugSettings.h
+++ Watchytchiuno/DebugSettings.h
@@ -1,4 +1,4 @@
-#define VERBOSE_LOGGING_ENABLED 1 // 1 to enable all DBGPrints, 0 to disable for performance
+#define VERBOSE_LOGGING_ENABLED 0 // 1 to enable all DBGPrints, 0 to disable for performance
 #define DISABLE_MAX_TIMESTAMP 0 // 1 to disable time delta clamping, allowing you to fast forward
 #define FORCED_NEXT_EVENT -1 // -1 to disable, 0+ to force a schedulable event (matching the SchedulableEvent enum)
 #define FORCED_ACTIVE_PLAYMATE -1 // -1 to disable, 0+ to force an active playmate (matching the PlaymateSpecies enum)
